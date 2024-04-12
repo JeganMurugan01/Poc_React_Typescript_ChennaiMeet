6ad4127c32124813159cc771a2b00d470139b07d
Jegan Murugan <jegan.murugan@concertidc.com>
2024-04-12 23:44:55 +0530

    file added

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -11,6 +11,8 @@ import {
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const Table = ({ rowData, colDefs }: any) => {
+  const containerStyle = useMemo(() => ({ width: "100%", height: "100%" }), []);
+  const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
   console.log(rowData, colDefs, "rowData, colDefs");
   const autoSizeStrategy = useMemo<
     | SizeColumnsToFitGridStrategy
@@ -23,24 +25,33 @@ const Table = ({ rowData, colDefs }: any) => {
   }, []);
   const defaultColDef = useMemo<ColDef>(() => {
     return {
-      editable: true,
       filter: true,
     };
+  }, []);
+
+  const popupParent = useMemo<HTMLElement | null>(() => {
+    return document.querySelector("body");
   }, []);
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const onGridReady = useCallback((params: any) => {
     console.log(params, "params");
   }, []);
   return (
-    <div className="ag-theme-quartz table-container">
-      <AgGridReact
-        rowData={rowData}
-        columnDefs={colDefs}
-        domLayout={"autoHeight"}
-        autoSizeStrategy={autoSizeStrategy}
-        onGridReady={onGridReady}
-        defaultColDef={defaultColDef}
-      />
+    <div style={containerStyle}>
+      <div style={gridStyle} className={"ag-theme-quartz-dark"}>
+        <AgGridReact
+          rowData={rowData}
+          columnDefs={colDefs}
+          domLayout={"autoHeight"}
+          autoSizeStrategy={autoSizeStrategy}
+          allowContextMenuWithControlKey={true}
+          suppressContextMenu={false}
+          enableRangeSelection={true}
+          popupParent={popupParent}
+          onGridReady={onGridReady}
+          defaultColDef={defaultColDef}
+        />
+      </div>
     </div>
   );
 };
