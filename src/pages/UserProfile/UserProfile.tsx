b496227363ea3aeaac5fb118eefe2ef31ed2b05c
Jegan Murugan <jegan.murugan@concertidc.com>
2024-04-22 11:53:44 +0530

    Checkbox added

--- src/pages/UserProfile/UserProfile.tsx
+++ src/pages/UserProfile/UserProfile.tsx
@@ -2,12 +2,29 @@ import { useLocation } from "react-router-dom";
 import { UserProfileLabel } from "../../constants";
 import { useGetUsersByIdQuery } from "../../redux/services/userServices/userService";
 import "../Admin/DashBoard/index.css";
+import { useState } from "react";
+import { FaEdit } from "react-icons/fa";
 export const UserProfile = () => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [userType, setUserType] = useState<string>("");
   const location = useLocation();
-  console.log(location.state.id);
   const { data } = useGetUsersByIdQuery({ id: location.state.id });
-  console.log(data, "data from the userProfile");
+  const [isUserCheckboxDisabled, setIsUserCheckboxDisabled] =
+    useState<boolean>(false);
+  const [isCoAdminCheckboxDisabled, setIsCoAdminCheckboxDisabled] =
+    useState<boolean>(false);
 
+  const handleCheckboxChange = (checkedType: string) => {
+    if (checkedType === "USER") {
+      setIsCoAdminCheckboxDisabled(true);
+      setIsUserCheckboxDisabled(false);
+      setUserType("USER");
+    } else {
+      setIsUserCheckboxDisabled(true);
+      setIsCoAdminCheckboxDisabled(false);
+      setUserType("COADMIN");
+    }
+  };
   return (
     <>
       <h4 className="d-flex justify-content-left mt-3 ms-3 ">
@@ -40,8 +57,40 @@ export const UserProfile = () => {
             <b>{data && data?.email}</b>
             <p className="m-0 mt-3">{UserProfileLabel?.PINCODE}</p>
             <b>{data && data?.profile?.pincode}</b>
-            <p className="m-0 mt-3">UserType:</p>
-            <b>{localStorage?.getItem("USERTYPE")}</b>
+            <p className="m-0 mt-3">UserType :</p>
+            {edit === false ? <b>{data && data?.UserType?.userType}</b> : ""}
+            {edit === false ? (
+              <>
+                <FaEdit
+                  className="ms-2 mb-1"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => setEdit(true)}
+                />
+              </>
+            ) : (
+              <>
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  id="USER"
+                  value="USER"
+                  checked={userType === "USER"}
+                  onChange={() => handleCheckboxChange("USER")}
+                  disabled={isUserCheckboxDisabled}
+                />
+                <label className="form-check-label ms-2">User</label>
+                <input
+                  className="form-check-input ms-5"
+                  type="checkbox"
+                  id="COADMIN"
+                  value="COADMIN"
+                  checked={userType === "COADMIN"}
+                  onChange={() => handleCheckboxChange("COADMIN")}
+                  disabled={isCoAdminCheckboxDisabled}
+                />
+                <label className="form-check-label ms-2">Co-Admin</label>
+              </>
+            )}
           </div>
         </div>
       </div>
